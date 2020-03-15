e446d5e01e13c076fba355f6b6bf59b8dd417abf
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-03-15 16:20:18 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Invoicing',
-    'version' : '1.1',
-    'summary': 'Invoices & Payments',
+    'name' : 'Registro',
+    'version' : '1.0',
+    'summary': 'Registro elettronico',
     'sequence': 15,
     'description': """
-Invoicing & Payments
+    Registro elettronico
 ====================
-The specific and easy-to-use Invoicing system in Odoo allows you to keep track of your accounting, even when you are not an accountant. It provides an easy way to follow up on your vendors and customers.
-
-You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
+Tentativo di realizzare un registro elettronico sulle ceneri del modulo di contabilita'.
     """,
-    'category': 'Invoicing Management',
+    'category': 'Scuola',
     'website': 'https://www.odoo.com/page/billing',
     'images' : ['images/accounts.jpeg','images/bank_statement.jpeg','images/cash_register.jpeg','images/chart_of_accounts.jpeg','images/customer_invoice.jpeg','images/journal_entries.jpeg'],
     'depends' : ['base_setup', 'product', 'analytic', 'portal', 'digest'],
