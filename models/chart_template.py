57709eb00aefb0ea43abafee438495c3b45b8185
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-03-16 16:38:34 +0100

    Update chart_template.py

--- models/chart_template.py
+++ models/chart_template.py
@@ -150,7 +150,7 @@ class AccountChartTemplate(models.Model):
                 break
         else:
             raise UserError(_('Cannot generate an unused account code.'))
-        current_assets_type = self.env.ref('account.data_account_type_current_assets', raise_if_not_found=False)
+        current_assets_type = self.env.ref('registro.data_account_type_current_assets', raise_if_not_found=False)
         return {
             'name': _('Liquidity Transfer'),
             'code': new_code,
