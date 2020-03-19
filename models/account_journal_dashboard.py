3bb4ba711da71bd6d460413d66bf243ebc3d1bd4
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-03-19 21:10:58 +0100

    Update account_journal_dashboard.py

--- models/account_journal_dashboard.py
+++ models/account_journal_dashboard.py
@@ -277,7 +277,7 @@ class account_journal(models.Model):
             ctx.update({'journal_type': self.type, 'default_type': 'out_invoice', 'type': 'out_invoice', 'default_journal_id': self.id})
             if ctx.get('refund'):
                 ctx.update({'default_type':'out_refund', 'type':'out_refund'})
-            view_id = self.env.ref('account.invoice_form').id
+            view_id = self.env.ref('registro.invoice_form').id
         elif self.type == 'purchase':
             ctx.update({'journal_type': self.type, 'default_type': 'in_invoice', 'type': 'in_invoice', 'default_journal_id': self.id})
             if ctx.get('refund'):
@@ -285,7 +285,7 @@ class account_journal(models.Model):
             view_id = self.env.ref('registro.invoice_supplier_form').id
         else:
             ctx.update({'default_journal_id': self.id, 'view_no_maturity': True})
-            view_id = self.env.ref('account.view_move_form').id
+            view_id = self.env.ref('registro.view_move_form').id
             model = 'account.move'
         return {
             'name': _('Create invoice/bill'),
@@ -408,11 +408,11 @@ class account_journal(models.Model):
     @api.multi
     def open_payments_action(self, payment_type, mode='tree'):
         if payment_type == 'outbound':
-            action_ref = 'account.action_account_payments_payable'
+            action_ref = 'registro.action_account_payments_payable'
         elif payment_type == 'transfer':
-            action_ref = 'account.action_account_payments_transfer'
+            action_ref = 'registro.action_account_payments_transfer'
         else:
-            action_ref = 'account.action_account_payments'
+            action_ref = 'registro.action_account_payments'
         [action] = self.env.ref(action_ref).read()
         action['context'] = dict(safe_eval(action.get('context')), default_journal_id=self.id, search_default_journal_id=self.id)
         if mode == 'form':
@@ -440,7 +440,7 @@ class account_journal(models.Model):
     @api.multi
     def create_bank_statement(self):
         """return action to create a bank statements. This button should be called only on journals with type =='bank'"""
-        action = self.env.ref('account.action_bank_statement_tree').read()[0]
+        action = self.env.ref('registro.action_bank_statement_tree').read()[0]
         action.update({
             'views': [[False, 'form']],
             'context': "{'default_journal_id': " + str(self.id) + "}",
